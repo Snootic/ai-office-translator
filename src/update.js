fbe5776b8f66c265e7fbf30771ab8e7a64ec0d90
Snootic <snooticonoob@hotmail.com>
2025-01-21 19:13:34 -0300

    feat: add download progress bar style and subtitle indicating newest version

--- src/update.js
+++ src/update.js
@@ -2,6 +2,7 @@ const { listen } = window.__TAURI__.event;
 
 const loadingSpinner = document.getElementById("loading-spinner");
 const statusText = document.getElementById("status");
+const latestVersionText = document.getElementById("latest-version");
 const progressBar = document.getElementById("progress-bar");
 const downloadProgress = document.getElementById("download-progress")
 
@@ -11,6 +12,8 @@ listen('update-progress', (event) => {
     loadingSpinner.classList.remove("show");
     
     downloadProgress.classList.add("show");
+
+    latestVersionText.innerHTML = `Newest version: ${event.payload.version}`;
 
     const { total_size, downloaded_size } = event.payload;
     const progressPercentage = (downloaded_size / total_size) * 100;
