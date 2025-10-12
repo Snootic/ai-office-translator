af0457de2c2ed1db5a3eec74fe47f3635f635b94
Snootic <snooticonoob@gmail.com>
2025-10-12 13:15:01 -0300

    refactor: remove deprecated get_chatgpt_keys and get_deep_keys commands; update handler references

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,29 +14,12 @@ use ai_translator;
 use std::{env, sync::Mutex};
 
 use documents::documents_handler;
-use get_api_keys::{get_deepl_keys, get_gpt_keys, Item};
 use glossary::glossary_handler;
 use tauri::Manager;
 use translate::translate_handler;
 use utils::utils_handler;
 
 use serde_json::Value;
-
-#[tauri::command]
-fn get_chatgpt_keys() -> Result<Vec<Item>, String> {
-    match crate::get_gpt_keys() {
-        Ok(keys) => Ok(keys),
-        Err(e) => Err(e.to_string()),
-    }
-}
-
-#[tauri::command]
-fn get_deep_keys() -> Result<Vec<Item>, String> {
-    match crate::get_deepl_keys() {
-        Ok(keys) => Ok(keys),
-        Err(e) => Err(e.to_string()),
-    }
-}
 
 fn main() {
     tauri::Builder::default()
@@ -116,12 +99,13 @@ fn main() {
             documents_handler::load_document,
             glossary_handler::get_glossaries,
             utils_handler::get_gpt_models,
+            utils_handler::get_gpt_billing,
             utils_handler::get_source_languages,
             utils_handler::get_target_languages,
             utils_handler::check_usage,
             translate_handler::translate_document,
-            get_chatgpt_keys,
-            get_deep_keys
+            get_api_keys::get_gpt_keys,
+            get_api_keys::get_deepl_keys
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
