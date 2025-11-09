f6643781cbc315772663e472bbe30b1f160ea02a
Snootic <snooticonoob@gmail.com>
2025-11-09 15:27:09 -0300

    feat: implement Glossary trait for Deepl and add create_glossary_from_excel method

--- src-tauri/src/models/deepl.rs
+++ src-tauri/src/models/deepl.rs
@@ -1,8 +1,16 @@
 use reqwest::{Error, Response};
-use serde_json::Value;
+use serde_json::{json, Value};
 use tauri::http::HeaderMap;
+use calamine::{Data, DataType, Reader, Xlsx, open_workbook};
 
 use super::model::APIClient;
+
+pub trait Glossary {
+  async fn create_glossary_from_excel(&self, excel_file_path: String) -> Value;
+  fn create_glossary(&self) -> Value;
+  fn load_json(&self, json: Value) -> Value;
+  fn get_glossaries(&self) -> Vec<Value>;
+}
 
 pub struct Deepl {
   client: APIClient,
@@ -45,4 +53,78 @@ impl Deepl {
 
     Ok(languages)
   }
+}
+
+impl Glossary for Deepl {
+  async fn create_glossary_from_excel(&self, excel_file_path: String) -> Value {
+      // I believe this works, couldn't test properly because I keep getting quota exceeded message
+      // Despite not using the API
+
+      let mut workbook: Xlsx<_> = open_workbook(&excel_file_path).expect("Cannot open file");
+
+      let mut filename = excel_file_path.split(".xls").next().unwrap_or("");
+      filename = filename.split("/").last().unwrap_or("");
+
+      let mut dictionaries: Vec<Value> = [].to_vec();
+
+      for sheet_name in workbook.sheet_names() {
+        let mut source_lang: String = String::new();
+        let mut target_lang: String = String::new();
+
+        let mut entries: String = String::new();
+
+        if let Ok(range) = workbook.worksheet_range(&sheet_name) {
+          for (row_index, row) in range.rows().enumerate() {
+            let non_empty_cells: Vec<&Data> = row.iter().filter(|cell| !cell.is_empty()).collect();
+            
+            if non_empty_cells.len() != 2 {
+              continue;
+            }
+
+            if row_index == 0 {
+              source_lang = row[0].as_string().unwrap_or_default();
+              target_lang = row[1].as_string().unwrap_or_default();
+              continue;
+            }
+
+            let col1 = row[0].as_string().unwrap_or_default();
+            let col2 = row[1].as_string().unwrap_or_default();
+
+            entries.push_str(&format!("{},{}\n", col1, col2));
+          }
+        }
+
+        let dict = json!({
+          "source_lang": source_lang,
+          "target_lang": target_lang,
+          "entries": entries,
+          "entries_format": "csv"
+        });
+
+        dictionaries.push(dict);
+      }
+
+      let body = json!({
+        "name": filename,
+        "dictionaries": dictionaries
+      });
+
+      let resp = self.client.post_json("https://api-free.deepl.com/v3/glossaries", &body).await;
+      
+      let resp: Value = resp.json().await.unwrap();
+      
+      resp
+    }
+  
+  fn create_glossary(&self) -> Value {
+        todo!()
+    }
+  
+  fn load_json(&self, json: Value) -> Value {
+        todo!()
+    }
+  
+  fn get_glossaries(&self) -> Vec<Value> {
+        todo!()
+    }
 }
